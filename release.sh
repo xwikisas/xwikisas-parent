a92907f398b22e99f5caf6966a05dd20ffb11859
Marius Dumitru Florea <marius@xwiki.com>
2018-02-09 18:44:16 +0200

    Improve the release script to match the one used on contrib parent.
    (cherry picked from commit 906081049bf8cdffb304a9c1e083242910836f2d)

--- release.sh
+++ release.sh
@@ -20,6 +20,23 @@
 # 02110-1301 USA, or see the FSF site: http://www.fsf.org.
 # ---------------------------------------------------------------------------
 
+# Collect the modules.
+modules=()
+currentFolderName=${PWD##*/}
+for module in *; do
+  # Skip files and symbolic links.
+  # Use only the folders that starts with '<currentFolderName>-'
+  if [[ -d $module && ! -L $module && $module == $currentFolderName-* ]]; then
+    modules+=("$module")
+  fi
+done
+
+function callForEachModule() {
+  for module in "${modules[@]}"; do
+    $1 "${module}"
+  done
+}
+
 function set_version() {
   cd $1
 
@@ -29,22 +46,31 @@ function set_version() {
   cd ..
 }
 
-function set_parent_version() {
-  cd $1
-
-  echo -e "\033[0;32m* Set parent version ${VERSION} in ${1}\033[0m"
-  xsltproc -o pom.xml --stringparam parentversion "${VERSION}" ../set-parent-version.xslt pom.xml
-
-  cd ..
-}
-
 function set_version_all() {
   echo              "*****************************"
   echo -e "\033[0;32m    Set version ${VERSION} in all pom files\033[0m"
   echo              "*****************************"
 
-  set_version xwikisas-parent-platform
-  set_version xwikisas-parent-platform-distribution
+  callForEachModule set_version
+
+  update_documentation
+}
+
+function set_parent_version() {
+  cd $1
+
+  echo -e "\033[0;32m* Set parent version ${PARENT_VERSION} in ${1}\033[0m"
+  mvn versions:update-parent -DgenerateBackupPoms=false -DparentVersion=[$PARENT_VERSION]
+
+  cd ..
+}
+
+function set_parent_version_all() {
+  echo              "*****************************"
+  echo -e "\033[0;32m    Set parent version ${PARENT_VERSION} in all pom files\033[0m"
+  echo              "*****************************"
+
+  callForEachModule set_parent_version
 }
 
 function commit_all() {
@@ -60,14 +86,14 @@ function tag_all() {
   echo -e "\033[0;32m    Create tag for new version\033[0m"
   echo              "*****************************"
 
-  git tag -m "Tagging ${parent-$VERSION}" xwikisas-parent-${VERSION}
+  git tag -m "Tagging ${currentFolderName}-${VERSION}" ${currentFolderName}-${VERSION}
 }
 
 function deploy_pom() {
   cd $1
 
-  echo -e "\033[0;32m* Deploy version ${1} ${VERSION}\033[0m"
-  mvn deploy -Dxwiki.enforcer.skip=true
+  echo -e "\033[0;32m* Deploy ${1} ${VERSION}\033[0m"
+  mvn deploy -Prelease-parent
 
   cd ..
 }
@@ -77,25 +103,67 @@ function deploy_all() {
   echo -e "\033[0;32m    Deploy all pom files\033[0m"
   echo              "*****************************"
 
-  deploy_pom xwikisas-parent-platform
-  deploy_pom xwikisas-parent-platform-distribution
+  callForEachModule deploy_pom
+}
+
+function update_documentation() {
+  echo              "*****************************"
+  echo -e "\033[0;32m    Update documentation\033[0m"
+  echo              "*****************************"
+
+  sed -i -e "s/$PREVIOUS_VERSION/$VERSION/g" README.md
 }
 
 # Check version to release
 if [[ -z $VERSION ]]
 then
+  cd "${modules[0]}"
+  current_version=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec)
+  cd ..
+  n=${current_version##*[!0-9]}
+  p=${current_version%%$n}
+  next_version=$p$((n+1))
+
   echo -e "Which version are you releasing?"
-  read -e -p "> " VERSION
+  read -e -p "> ($next_version) " tmp
+  if [[ $tmp ]]
+  then
+    next_version=$tmp
+  fi
+
+  export PREVIOUS_VERSION=$current_version
+  export VERSION=$next_version
+
   echo -n -e "\033[0m"
-  export VERSION=$VERSION
+fi
+
+# Check parent version to update
+if [[ -z $PARENT_VERSION ]]
+then
+  cd "${modules[0]}"
+  parent_version=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.parent.version}' --non-recursive exec:exec)
+  cd ..
+
+  echo -e "Do you want to change the parent version?"
+  read -e -p "> ($parent_version) " tmp
+  if [[ $tmp ]]
+  then
+    export PARENT_VERSION=$tmp
+  fi
+
+  echo -n -e "\033[0m"
 fi
 
 set -e
 
 set_version_all
+if [[ ! -z $PARENT_VERSION ]]; then
+  set_parent_version_all
+fi
 commit_all
 tag_all
 deploy_all
 
 echo -e "\033[0;32m Push changes and tag\033[0"
 git push --tags
+git push
